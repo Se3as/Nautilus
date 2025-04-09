55eecdd13292e0a44fc78ee0f4e6a7bf926f1ed6
Carlos Chavarria <carlos.chavarriaquiros@ucr.ac.cr>
2025-04-09 01:01:03 -0600

    organize merge

---
+++ include/Player.h
@@ -0,0 +1,21 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <iostream>
+#include <string>
+
+
+class Player {
+  private:
+    std::string name;
+    int actionPoints;
+    int money;
+    int score;
+  public:
+    Player();
+    Player(std::string);
+    std::string getName();
+    int getScore();
+    void setScore(int);
+    ~Player();
+};
+#endif
