3e102187e7ea3873d695f72781cdab3147cda34b
Carlos Andres Chavarria Quiros <carlos.chavarriaquiros@ucr.ac.cr>
2025-04-15 21:08:29 -0600

    fix parenthesis

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,52 +2,60 @@
 #include <iostream>
 #include <string>
 
-class Player {
-  private:
-    bool playerID;
-    int actionPoints;
-    int coins;
-    int score;
-  public:
-    Player(int numJugador){
-        if(int numJugador =! 1){ {
-            playerID = true;
-        } else {
-            playerID = 0;
-        }
-        this->playerID = false;
-        this->actionPoints = 0;
-        this->coins = 0;
-        this->score = 0;
+class Player
+{
+private:
+  bool playerID;
+  int actionPoints;
+  int coins;
+  int score;
+
+public:
+  Player(int numJugador)
+  {
+    if (int numJugador = !1)
+    {
+
+      playerID = true;
     }
-    Player(std::string);
-    std::string getName();
-    int getScore(){
-        return score;
+    else
+    {
+      playerID = 0;
     }
-    void setScore(int){
-        std::cout << "Score set to: " << score << std::endl;
-        this->score = score;
+    this->playerID = false;
+    this->actionPoints = 0;
+    this->coins = 0;
+    this->score = 0;
+  }
+    bool getPlayerID()
+    {
+      return playerID;
+    }
+    int getScore()
+    {
+      return score;
+    }
+    void setScore(int)
+    {
+      std::cout << "Score set to: " << score << std::endl;
+      this->score = score;
     }
 
     ~Player();
-    
-    bool calculatePoints(cost){
 
-        if (cost > coins) {
-            std::cout << "Not enough points" << std::endl;
-            return false;
-        } else {
-            coins -= cost;
-            std::cout << "Points deducted: " << cost << std::endl;
-            return true;
-        }
+    bool calculatePoints(int cost)
+    {
 
-
+      if (cost > coins)
+      {
+        std::cout << "Not enough points" << std::endl;
+        return false;
+      }
+      else
+      {
+        coins -= cost;
+        std::cout << "Points deducted: " << cost << std::endl;
+        return true;
+      }
     }
-    
-    
-
-
-
-    
+  };
